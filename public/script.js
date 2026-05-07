d151cd1b01cfa2e7c49b5c1a9c93da645cbf5695
Anna Luiza Pereirs Silva <annalumodascolonial@gmail.com>
2026-05-07 16:11:18 -0300

    git commit -m Atividade Prática - Funções e DOM - matrícula:1656540

--- public/script.js
+++ public/script.js
@@ -1,25 +1,119 @@
 const data = {
-    "produtos": [
-      {
-        "id": 1,
-        "nome": "Smartphone Galaxy S23",
-        "preco": 3499.90,
-        "categoria": "Celulares",
-        "imagem": "https://example.com/imagens/galaxy-s23.jpg",
-        "descricao": "Smartphone com 128GB de armazenamento, câmera de alta resolução e excelente desempenho.",
-        "emEstoque": true
-      },
-      {
-        "id": 2,
-        "nome": "Notebook Dell Inspiron 15",
-        "preco": 4599.00,
-        "categoria": "Notebooks",
-        "imagem": "https://example.com/imagens/dell-inspiron-15.jpg",
-        "descricao": "Notebook com processador Intel i7, 16GB de RAM e SSD de 512GB, ideal para trabalho e estudos.",
-        "emEstoque": false
+  produtos: [
+    {
+      id: 1,
+      nome: "Calça Cargo",
+      preco: 199.90,
+      categoria: "Feminino",
+      descricao: "Calça Cargo Estilosa para melhorar o look.",
+      emEstoque: true
+    },
+
+    {
+      id: 2,
+      nome: "Vestido Rosa",
+      preco: 299.90,
+      categoria: "Feminino",
+      descricao: "Vestido rosa romântico e elegante para diversas ocasiões.",
+      emEstoque: true
+    },
+
+    {
+      id: 3,
+      nome: "Conjunto Blazer Casual",
+      preco: 469.90,
+      categoria: "Feminino",
+      descricao: "Conjunto confortável e estiloso.",
+      emEstoque: true
+    },
+
+    {
+      id: 4,
+      nome: "Conjunto Branco",
+      preco: 299.90,
+      categoria: "Feminino",
+      descricao: "Conjunto short e camisa despojado.",
+      emEstoque: false
+    },
+
+    {
+      id: 5,
+      nome: "Vestido Festa",
+      preco: 89.90,
+      categoria: "Feminino",
+      descricao: "Vestido festa brilho e elegante.",
+      emEstoque: true
+    },
+
+    {
+      id: 6,
+      nome: "Óculos de Sol Vintage",
+      preco: 129.90,
+      categoria: "Acessórios",
+      descricao: "Óculos de Sol Vintage e estiloso",
+      emEstoque: true
+    },
+
+    {
+      id: 7,
+      nome: "Óculos de Sol Marilia",
+      preco: 109.90,
+      categoria: "Acessórios",
+      descricao: "Óculos de Sol moderno com acabamento premium.",
+      emEstoque: false
+    },
+
+    {
+      id: 8,
+      nome: "Lenço Maria",
+      preco: 89.90,
+      categoria: "Acessórios",
+      descricao: "Lenço estiloso para completar seu look",
+      emEstoque: true
+    }
+  ]
+};
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const searchInput = document.getElementById("search");
+  const selectCategory = document.getElementById("category");
+  const btnSearch = document.querySelector(".btn-render");
+  const products = document.querySelectorAll(".card-produto");
+  const btnBanner = document.querySelector(".btn");
+function filterProducts() {
+    const searchText = searchInput.value.toLowerCase();
+
+    products.forEach((product) => {
+      const title = product.querySelector("h3").textContent.toLowerCase();
+
+      if (title.includes(searchText)) {
+        product.style.display = "block";
+      } else {
+        product.style.display = "none";
       }
-    ]
+    });
   }
+  btnSearch.addEventListener("click", filterProducts);
+  searchInput.addEventListener("keyup", filterProducts);
+   btnBanner.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(".catalogo").scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+  products.forEach((product) => {
+    const btn = product.querySelector("button");
+
+    btn.addEventListener("click", function () {
+      alert("Produto adicionado ao carrinho!");
+    });
+  });
+
+});
 
 
   
